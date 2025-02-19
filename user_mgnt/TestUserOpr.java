5cc2a60dbce97c04dcc097cf2bd229b2665a3aaa
Shamsaagazarzoo Alam <arzoo.rkcet@gmail.com>
2025-02-19 10:53:20 +0530

    Update TestUserOpr.java

--- user_mgnt/TestUserOpr.java
+++ user_mgnt/TestUserOpr.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class TestUserOpr {
     public static void main(String[] args) {
         UserOperations uopr = new UserOperations();
-        uopr.addUser();
-        User[] users = uopr.listUsers();
-        for (User user : users) {
-            System.out.println(user);
+        System.out.println("Enter 1 for add");
+        System.out.println("Enter 2 for update");
+        System.out.println("Enter 3 for delete");
+        System.out.println("Enter 4 for list all user");
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        sc.nextLine();
+
+        switch (choice) {
+            case 1:
+                uopr.addUser();
+                break;
+            case 2:
+                uopr.updateUser();
+                break;
+            case 3:
+                uopr.deleteUser();
+                break;
+            case 4:
+                User[] users = uopr.listUsers();
+                for (User user : users) {
+                    System.out.println(user);
+                }
+                break;
+            default:
+                System.out.println("Invalid choice");
+                ;
+                break;
         }
 
     }
